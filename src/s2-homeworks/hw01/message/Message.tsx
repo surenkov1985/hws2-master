9decf9e38c292628ca3a62d3c388651a304aaf1e
mich-man <88162731+surenkov1985@users.noreply.github.com>
2024-07-17 21:46:51 +0300

    start layout

--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -1,37 +1,30 @@
 import React from 'react'
 import s from './Message.module.css'
+import {MessageType} from "../HW1";
 
 // нужно создать правильный тип вместо any
-export type MessagePropsType = any
+export type MessagePropsType = { message: MessageType }
 
 // нужно отобразить приходящие данные
-const Message = (props: MessagePropsType) => {
+const Message = ({message}: MessagePropsType) => {
     return (
-        <div id={'hw1-message-' + props.message.id} className={s.message}>
+        <div id={'hw1-message-' + message.id} className={s.message}>
             <div className={s.imageAndText}>
                 <img
-                    id={'hw1-avatar-' + props.message.id}
-                    // создаёт студент
-
-                    //
+                    id={'hw1-avatar-' + message.id}
+                    src={message.user.avatar}
                 />
                 <div className={s.text}>
-                    <div id={'hw1-name-' + props.message.id} className={s.name}>
-                        {/*создаёт студент*/}
-
-                        {/**/}
+                    <div id={'hw1-name-' + message.id} className={s.name}>
+                        {message.user.name}
                     </div>
-                    <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
-                        {/*создаёт студент*/}
-
-                        {/**/}
+                    <pre id={'hw1-text-' + message.id} className={s.messageText}>
+                        {message.message.text}
                     </pre>
                 </div>
             </div>
-            <div id={'hw1-time-' + props.message.id} className={s.time}>
-                {/*создаёт студент*/}
-
-                {/**/}
+            <div id={'hw1-time-' + message.id} className={s.time}>
+                {message.message.time}
             </div>
         </div>
     )
